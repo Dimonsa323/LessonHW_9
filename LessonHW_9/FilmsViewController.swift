a6fc409f78cadca825593fc7cb0d25b5f9689de4
Dima Hubenia <dima96gubenya@gmail.com>
2022-08-03 08:43:00 +0300

    Доп модель

--- LessonHW_9/FilmsViewController.swift
+++ LessonHW_9/FilmsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FirstScreen: UIViewController {
+class FilmsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- LessonHW_9/Models/User.swift
+++ LessonHW_9/Models/User.swift
@@ -6,9 +6,19 @@
 //
 
 import Foundation
+import UIKit
 
 struct User {
     var name: String
     var password: String
+    var film: String
+    var filmImage: UIImage
+    var tvSeries: String
+    var releaseDate: Int
 }
 
+extension User {
+    static func getInfoList() -> [User] {
+        
+    }
+}
